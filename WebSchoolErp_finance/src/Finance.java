13dde20e9755c2e1bf9cf0e8c2ca1efacd8e254c
vinay kumar <namagondlavinay@gmail.com>
2019-04-19 11:22:02 +0530

    second commit

--- WebSchoolErp_finance/src/Finance.java
+++ WebSchoolErp_finance/src/Finance.java
@@ -3,16 +3,25 @@ public class Finance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//first commit
 		int i = 0;
 		System.out.println(++i);
 		int k = 1;
 		System.out.println(--k);
 		int j = 0;
-		/*
-		 System.out.println(j++);
+		System.out.println(j++);
 		System.out.println(j);
 	System.out.println("...............................");
-	*/
+	//second commit
+	int n = 0;
+	int m = n++ + ++n;
+	System.out.println(m);
+	System.out.println("...............................");
+	int g = 0;
+	int r = g++ + g++;
+	System.out.println(r);
+	System.out.println("...............................");
+	
 	}
 	
 
