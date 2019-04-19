44107df85a50e50255756d576f0c60bf2d2669b4
vinay kumar <namagondlavinay@gmail.com>
2019-04-19 11:14:20 +0530

    first commit

---
+++ WebSchoolErp_finance/src/Finance.java
@@ -0,0 +1,19 @@
+
+public class Finance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println(++i);
+		int k = 1;
+		System.out.println(--k);
+		int j = 0;
+		/*
+		 System.out.println(j++);
+		System.out.println(j);
+	System.out.println("...............................");
+	*/
+	}
+	
+
+}
